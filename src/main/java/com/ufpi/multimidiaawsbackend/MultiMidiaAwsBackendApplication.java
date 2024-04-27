b1bec5ecbe13ec38c211558ca37982df310ca438
Node-23 <matheus.dev@hotmail.com>
2024-04-27 11:35:24 -0300

    first commit

---
+++ src/main/java/com/ufpi/multimidiaawsbackend/MultiMidiaAwsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ufpi.multimidiaawsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiMidiaAwsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiMidiaAwsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufpi/multimidiaawsbackend/MultiMidiaAwsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpi.multimidiaawsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiMidiaAwsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
